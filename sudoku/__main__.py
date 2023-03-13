fe58be7647ab094dbe4d65518cbeb46306d295f9
bobbarnes1981 <robertbarnes1981@googlemail.com>
2023-03-13 13:55:50 +0000

    Implement row/col/subgrid checking

--- sudoku/__main__.py
+++ sudoku/__main__.py
@@ -1,12 +1,20 @@
+import logging
 import pygame
 import time
 from pygame.locals import *
 
+logging.basicConfig(level=logging.INFO)
+
 cell_width = 20
 cell_height = 20
-BACK_COL = (80,80,80)
-TEXT_COL = (25,25,112)
+COL_BACK = (0,0,0)
+COL_TEXT = (25,25,112)
+COL_YELLOW = (255,255,0)
 DELAY = 0.5
+CELL_THICKNESS = 1
+GRID_THICKNESS = 5
+
+# TODO: incorporate cell wall thickness into render
 
 TEST_DATA = [
     [0,5,0,3,0,0,0,6,0],
@@ -24,119 +32,204 @@ class App(object):
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self._size = self.width, self.height = cell_width*9*3, cell_height*9*3
+        self._size = ((cell_width*9*3), (cell_height*9*3))
         self._grid = Grid()
         self._time = time.time()
         self._counter = 0
         self._checking = "ROW"
         self._counter_row = -1
         self._counter_col = -1
+        self._counter_sub_row = -1
+        self._counter_sub_col = -1
         self._check_cell_row = 0
         self._check_cell_col = 0
+        self._render_check = True
+        logging.info('Loading grid')
         for row in range(0, 9):
             for col in range(0, 9):
                 if TEST_DATA[row][col] != 0:
                     self._grid.set(row, col, TEST_DATA[row][col])
-#                    self._grid.check(row, col)
-        
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self._size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
         #self.font = pygame.font.SysFont('courier.ttf', 72)
         font_name = pygame.font.get_default_font()
-        print("System font: {0}".format(font_name))
+        logging.info("System font: {0}".format(font_name))
         self.font_s = pygame.font.SysFont(None, 22)
         self.font_l = pygame.font.SysFont(None, 66)
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self.running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == 27:
+                self._running = False
+        else:
+            logging.debug(event)
     def on_loop(self, elapsed):
         self._counter+=elapsed
         if self._counter > DELAY:
-            self._counter = 0;
+            logging.info("tick")
+            self._counter = 0
             self.check_grid()
     def check_grid(self):
-        print(self._check_cell_row, self._check_cell_col)
+        logging.info('Check grid')
+        if self._grid.is_complete_grid():
+            return
         finished_check = self.check_cell()
+        logging.info('finished check {0}'.format(finished_check))
         if finished_check:
             self._check_cell_col+=1
+            logging.info('increment col')
             if self._check_cell_col >= 9:
                 self._check_cell_col = 0
                 self._check_cell_row+=1
+                logging.info('increment row')
                 if self._check_cell_row >= 9:
                     self._check_cell_row = 0
     def check_cell(self):
-        if not self._grid.is_complete(self._check_cell_row, self._check_cell_col):
+        logging.info('Check cell')
+        if not self._grid.is_complete_cell(self._check_cell_row, self._check_cell_col):
             return True
-        # row
-        if self._checking == "ROW":
-            self._counter_row += 1
-            if self._counter_row >= 9:
-                self._counter_row = -1
-                self._checking = "COL"
-            if self._check_cell_row != self._counter_row:
-                print("\tcheck {0}, count {1}".format(self._check_cell_row, self._counter_row))
-                print(self._grid.get(self._check_cell_row, self._check_cell_col))
-                if not self._grid.is_complete(self._counter_row, self._check_cell_col):
-                    self._grid.remove(self._counter_row, self._check_cell_col, self._grid.get(self._check_cell_row, self._check_cell_col))
         # col
-        if self._checking == "COL":
+        if self._checking == "ROW":
+            logging.info('Check row')
+            self._render_check = True
+            if self._grid.is_complete_row(self._check_cell_row):
+                self._checking = "COL"
+                self._render_check = False
+                return False
             self._counter_col += 1
             if self._counter_col >= 9:
                 self._counter_col = -1
+                self._checking = "COL"
+            if self._check_cell_col != self._counter_col:
+                if not self._grid.is_complete_cell(self._check_cell_row, self._counter_col):
+                    self._grid.remove(self._check_cell_row, self._counter_col, self._grid.get(self._check_cell_row, self._check_cell_col))
+        # row
+        if self._checking == "COL":
+            logging.info('Check col')
+            self._render_check = True
+            if self._grid.is_complete_col(self._check_cell_col):
                 self._checking = "SUB"
+                self._render_check = False
+                return False
+            self._counter_row += 1
+            if self._counter_row >= 9:
+                self._counter_row = -1
+                self._checking = "SUB"
+            if self._check_cell_row != self._counter_row:
+                if not self._grid.is_complete_cell(self._counter_row, self._check_cell_col):
+                    self._grid.remove(self._counter_row, self._check_cell_col, self._grid.get(self._check_cell_row, self._check_cell_col))
         # subgrid
         if self._checking == "SUB":
-            # todo: cycle cells,then return True
-            self._checking = "ROW"
-            return True
-        # completed
+            logging.info('Check sub')
+            self._render_check = True
+            if self._grid.is_complete_sub(self._check_cell_row, self._check_cell_col):
+                self._checking = "ROW"
+                self._render_check = False
+                # completed
+                return True
+            self._counter_sub_col += 1
+            if self._counter_sub_col >= 3:
+                self._counter_sub_col = -1
+                self._counter_sub_row += 1
+                if self._counter_sub_row >= 3:
+                    self._counter_sub_row = -1
+                    self._checking = "ROW"
+                    # completed
+                    return True
+            if self._counter_sub_col == -1:
+                self._counter_sub_col+=1
+            if self._counter_sub_row == -1:
+                self._counter_sub_row+=1
+            (row_val, col_val) = self.get_sub_vals()
+            if self._check_cell_row != row_val or self._check_cell_col != col_val:
+                if not self._grid.is_complete_cell(row_val, col_val):
+                    self._grid.remove(row_val, col_val, self._grid.get(self._check_cell_row, self._check_cell_col))
+        # not completed
         return False
+    def get_sub_vals(self):
+        col_offset = (self._check_cell_col // 3) * 3
+        row_offset = (self._check_cell_row // 3) * 3
+        col_val = col_offset + self._counter_sub_col
+        row_val = row_offset + self._counter_sub_row
+        return (row_val, col_val)
     def on_render(self):
-        self._display_surf.fill(BACK_COL)
-        self.draw_cell()
-        self.draw_checking()
+        self._display_surf.fill(COL_BACK)
+        self.draw_cells()
         self.draw_lines()
-        self.draw_grid()
+        self.draw_check_cell()
+        self.draw_checking()
+        self.draw_numbers()
         pygame.display.update()
-    def draw_cell(self):
+    def draw_check_cell(self):
         left = cell_width*3*self._check_cell_col
         top = cell_height*3*self._check_cell_row
         width = cell_width*3
         height = cell_height*3
-        pygame.draw.rect(self._display_surf, (255,255,0), (left, top, width, height), 0)
+        pygame.draw.rect(self._display_surf, COL_YELLOW, (left, top, width, height), 2)
     def draw_checking(self):
-        if self._checking == "ROW":
+        if not self._render_check:
+            return
+        if self._checking == "COL":
             left = cell_width*3*self._check_cell_col
             top = cell_height*3*self._counter_row
-        if self._checking == "COL":
+        if self._checking == "ROW":
             left = cell_width*3*self._counter_col
             top = cell_height*3*self._check_cell_row
+        if self._checking == "SUB":
+            (row_val, col_val) = self.get_sub_vals()
+            left = cell_width*3*col_val
+            top = cell_height*3*row_val
         width = cell_width*3
-        height = cell_height*3           
+        height = cell_height*3
         pygame.draw.rect(self._display_surf, (0,255,0), (left, top, width, height), 0)
-            
     def draw_lines(self):
+        right = (cell_width*9*3)
+        bottom = (cell_height*3*9)
+        for row in range(1, 9):
+            v_line = (cell_height*3*row)
+            pygame.draw.line(self._display_surf, (255,0,0), (0,v_line), (right,v_line), GRID_THICKNESS if row % 3 == 0 else CELL_THICKNESS)
+            for col in range(1, 9):
+                h_line = (cell_width*3*col)
+                pygame.draw.line(self._display_surf, (255,0,0), (h_line, 0), (h_line,bottom), GRID_THICKNESS if col % 3 == 0 else CELL_THICKNESS)
+    def draw_numbers(self):
         for row in range(0, 9):
-            pygame.draw.line(self._display_surf, (255,0,0), (0,cell_height*3*row), (cell_width*9*3,cell_height*3*row), 5 if row % 3 == 0 else 1)
             for col in range(0, 9):
-                pygame.draw.line(self._display_surf, (255,0,0), (cell_width*3*col, 0), (cell_width*3*col,cell_height*3*9), 5 if col % 3 == 0 else 1)
-    def draw_grid(self):
+                self.draw_number(row, col)
+    def draw_number(self, row, col):
+        x = (cell_width*3*col)
+        y = (cell_height*3*row)
+        if self._grid.is_complete_cell(row, col):
+            x += 15
+            y += 10
+            img = self.font_l.render(str(self._grid.get(row, col)), True, COL_TEXT)
+            self._display_surf.blit(img, (x, y))
+        else:
+            x += 5
+            y += 4
+            count = 1
+            for subrow in range(0, 3):
+                for subcol in range(0, 3):
+                    img = self.font_s.render(str(count if self._grid.has(row, col, count) else ' '), True, COL_TEXT)
+                    self._display_surf.blit(img, (x+(subcol*cell_width), y+(subrow*cell_height)))
+                    count+=1
+    def draw_cells(self):
         for row in range(0, 9):
             for col in range(0, 9):
-                x = cell_width*3*col
-                y = cell_height*3*row
-                if self._grid.is_complete(row, col):
-                    img = self.font_l.render(str(self._grid.get(row, col)), True, TEXT_COL)
-                    self._display_surf.blit(img, (x, y))
-                else:
-                    count = 1
-                    for subrow in range(0, 3):
-                        for subcol in range(0, 3):
-                            img = self.font_s.render(str(count), True, TEXT_COL)
-                            self._display_surf.blit(img, (x+(subcol*cell_width), y+(subrow*cell_height)))
-                            count+=1
+                self.draw_cell(row, col)
+            #exit()
+    def draw_cell(self, row, col):
+        if self._grid.is_complete_cell(row, col):
+            colour = (0,0,0)
+        else:
+            colour = (80,80,80)
+        left = (cell_width*3*(col))
+        top = (cell_height*3*(row))
+        width = cell_width*3
+        height = cell_height*3
+        pygame.draw.rect(self._display_surf, colour, (left, top, width, height), 0)
     def on_cleanup(self):
         pygame.quit()
     def on_execute(self):
@@ -160,36 +253,40 @@ class Grid(object):
             for col in range(0, 9):
                 self._cells[row].append([])
                 self._cells[row][col] = Cell()
-    def is_complete(self, row, col):
+    def is_complete_cell(self, row, col):
         return self._cells[row][col].is_complete()
+    def is_complete_row(self, row):
+        for i in range(0, 9):
+            if not self._cells[row][i].is_complete():
+                return False
+        return True
+    def is_complete_col(self, col):
+        for i in range(0, 9):
+            if not self._cells[i][col].is_complete():
+                return False
+        return True
+    def is_complete_sub(self, row, col):
+        col_offset = (col // 3) * 3
+        row_offset = (row // 3) * 3
+        for r in range(0, 3):
+            for c in range(0, 3):
+                if not self._cells[row_offset+r][col_offset+c].is_complete():
+                    return False
+        return True
+    def is_complete_grid(self):
+        for r in range(0, 9):
+            for c in range(0, 9):
+                if not self._cells[r][c].is_complete():
+                    return False
+        return True
+    def has(self, row, col, val):
+        return self._cells[row][col].has(val)
     def set(self, row, col, val):
         self._cells[row][col].set(val)
     def get(self, row, col):
         return self._cells[row][col].get()
     def remove(self, row, col, val):
         self._cells[row][col].remove(val)
-#    def check(self, row, col):
-#        val = self._cells[row][col].get()
-#        self.check_row(row, col, val)
-#        self.check_col(row, col, val)
-#        self.check_subgrid(row, col, val)
-#    def check_row(self, row, col, val):
-#        for c in range(0, 9):
-#            if c != col:
-#                self._cells[row][c].remove(val)
-#    def check_col(self, row, col, val):
-#        for r in range(0, 9):
-#            if r != row:
-#                self._cells[r][col].remove(val)
-#    def check_subgrid(self, row, col, val):
-#        r_min = (row % 3) * 3
-#        r_max = r_min + 3
-#        c_min = (col % 3) * 3
-#        c_max = c_min + 3
-#        for r in range(r_min, r_max):
-#            for c in range(c_min, c_max):
-#                if r != row or c != col:
-#                    self._cells[r][c].remove(val)
 class Cell(object):
     def __init__(self):
         self._numbers = []
@@ -197,30 +294,26 @@ class Cell(object):
             self._numbers.append(True)
     def is_complete(self):
         return self._numbers.count(True) == 1
+    def has(self, val):
+        if val not in range(1, 10):
+            raise Exception("invalid val {0}".format(val))
+        return self._numbers[val-1]
     def set(self, val):
-        count = 1
+        if val not in range(1, 10):
+            raise Exception("invalid val {0}".format(val))
         for i in range(0, 9):
-            if count != val:
-                self._numbers[i] = False
-            else:
+            if i == val-1:
                 self._numbers[i] = True
-            count+=1
+            else:
+                self._numbers[i] = False
     def get(self):
         if not self.is_complete():
             return 0
-        count = 1
-        for i in range(0, 9):
-            if self._numbers[i] == True:
-                return count
-            count+=1
-        return -1
+        return self._numbers.index(True)+1
     def remove(self, val):
-        count = 1
-        for i in range(0, 9):
-            if count == val:
-                self._numbers[i] = False
-                break
-            count+=1
+        if val not in range(1, 10):
+            raise Exception("invalid val {0}".format(val))
+        self._numbers[val-1] = False
 
 if __name__ == '__main__':
     a = App()
